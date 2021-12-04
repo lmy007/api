0196c67afda9c84c67859f024fa2899533f63250
lmy007 <kimliu666@gmail.com>
2021-12-04 21:56:14 +0800

    revise static dir

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import router from './router/index.js'
 const app = express()
+app.use(express.static('public'))
 app.use('/api', router)
-
 app.listen(4000, () => {
   console.log('api server is running...')
 })
--- controllers/shopcart_ctrl.js
+++ controllers/shopcart_ctrl.js
@@ -17,4 +17,3 @@ export async function getGoods(req, res) {
     })
   }
 }
-
