7c752157f4e080ab3884233abf14838b6a83ce12
lmy007 <kimliu666@gmail.com>
2021-12-05 00:23:49 +0800

    change data -> list

--- controllers/shopcart_ctrl.js
+++ controllers/shopcart_ctrl.js
@@ -4,10 +4,11 @@ export async function getGoods(req, res) {
     const sql = 'SELECT * FROM shopcart'
     const sqlArr = []
     const data = await sqlConnection(sql, sqlArr)
+    res.setHeader("Access-Control-Allow-Origin", "*")
     res.send({
       status: 200,
-      messeage: '獲取列表成功',
-      data: data
+      message: '獲取列表成功',
+      list: data
     })
   } catch (e) {
     res.send({
